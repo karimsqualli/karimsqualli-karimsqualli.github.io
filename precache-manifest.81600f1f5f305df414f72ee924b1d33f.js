33a61eae1eb3d17243ac9de4f4ab2d1a25bae0ad
karimsqualli <karim.squalli@usmba.ac.ma>
2019-08-31 11:00:50 +0200

    first commit

---
+++ precache-manifest.81600f1f5f305df414f72ee924b1d33f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "80dfdf39974408a69d1d",
+    "url": "/js/app.95b64e86.js"
+  },
+  {
+    "revision": "59dc451ec13205cd304b",
+    "url": "/css/chunk-vendors.908b6f6a.css"
+  },
+  {
+    "revision": "59dc451ec13205cd304b",
+    "url": "/js/chunk-vendors.812fe2e8.js"
+  },
+  {
+    "revision": "c68898981f08bdc8b3384285ab952e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbe982f4eeb1fbf29cf73f49b2a5cc4",
+    "url": "/img/0.jpeg"
+  },
+  {
+    "revision": "8417422099aeefa7a7ec8d43278d7fb0",
+    "url": "/img/1.jpeg"
+  },
+  {
+    "revision": "e67eec4979609fbe7aab8b979792cdaa",
+    "url": "/img/3.jpeg"
+  },
+  {
+    "revision": "e8496a4e04ce96982910e3f1698895d5",
+    "url": "/img/2.jpeg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c7600d7f4cf2960a9b79a3da4db35ecb",
+    "url": "/style.css"
+  }
+];
